b33ac8fbaddbbb27c4ca3a8f117f2cab4f102041
agiratech-nagaraj <nagaraj@agiratech.com>
2020-05-31 17:39:40 +0530

    chore: Add android files

---
+++ android/app/src/main/java/com/agiratect/anet/MainActivity.java
@@ -0,0 +1,36 @@
+package com.agiratect.anet;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.ServiceWorkerClient;
+import android.webkit.ServiceWorkerController;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+
+import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+
+import java.util.ArrayList;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    if(Build.VERSION.SDK_INT >= 24 ){
+      ServiceWorkerController swController = ServiceWorkerController.getInstance();
+
+      swController.setServiceWorkerClient(new ServiceWorkerClient() {
+        @Override
+        public WebResourceResponse shouldInterceptRequest(WebResourceRequest request) {
+          return bridge.getLocalServer().shouldInterceptRequest(request);
+        }
+      });
+    }
+    // Initializes the Bridge
+    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+      // Additional plugins you've installed go here
+      // Ex: add(TotallyAwesomePlugin.class);
+    }});
+  }
+}
