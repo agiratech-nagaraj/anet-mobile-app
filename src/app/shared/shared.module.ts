ee877dc88b3189541245b9c202bfe0faf1462a1d
agiratech-nagaraj <nagaraj@agiratech.com>
2020-06-13 17:49:39 +0530

    refactor: split the skeleton list templates

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {IonicModule} from '@ionic/angular';
+
+import {SkeletonListComponent} from './components/skeleton-list/skeleton-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SkeletonListComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
+  ],
+  exports: [
+    SkeletonListComponent
   ]
 })
 export class SharedModule { }
