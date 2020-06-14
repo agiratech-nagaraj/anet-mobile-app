0590a0ff6dc551d5aa54c2af788b39502297487f
agiratech-nagaraj <nagaraj@agiratech.com>
2020-06-14 20:34:08 +0530

    feat: Added charts tab

--- src/app/pages/tabs/tabs.module.ts
+++ src/app/pages/tabs/tabs.module.ts
@@ -12,11 +12,12 @@ const routes: Routes = [
     path: '',
     component: TabsPage,
     children: [
-      {path: '', redirectTo: 'logtime', pathMatch: 'full'},
+      {path: '', redirectTo: 'charts', pathMatch: 'full'},
      { path: 'timesheets', loadChildren: () => import('../time-sheets/time-sheets.module').then(m => m.TimeSheetsPageModule) },
      { path: 'applied_wfh', loadChildren: () => import('../applied-wfh/applied-wfh.module').then(m => m.AppliedWfhPageModule) },
      { path: 'wfh', loadChildren: () => import('../wfh/wfh.module').then(m => m.WfhPageModule) },
      { path: 'logtime', loadChildren: () => import('../log-time/log-time.module').then(m => m.LogTimePageModule) },
+     { path: 'charts', loadChildren: () => import('../home/home.module').then(m => m.HomePageModule) },
     ]
   }
 ];
